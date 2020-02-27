f2c317316f20b5bb3fee252181db39929368eb75
Eduardo Aguirre Garza <eduardo.agarza@hotmail.com>
2020-02-27 15:13:59 -0700

    testt

--- HR Job Requirements/Agregar_objetivo.vb
+++ HR Job Requirements/Agregar_objetivo.vb
@@ -13,7 +13,7 @@
             Me.Text = "Editar objetivo"
             CargarInformacion(id)
         End If
-
+        ''test github
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
